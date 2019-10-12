cbc41d6aa2f095f74f070c62f0846de7b9abc955
bobkrstic <slobodankrsticnis@yahoo.com>
2019-10-12 13:35:29 -0700

    Refactoring with Router

--- app.js
+++ app.js
@@ -13,76 +13,15 @@ app.use(express.static("./public"));
 
 app.use(morgan("short"));
 
-app.post("/user_create", (req, res) => {
-  console.log("Trying to create a new user...");
-  console.log("First name: " + req.body.create_first_name);
-  // capturing input from a user
-  const firstName = req.body.create_first_name;
-  const lastName = req.body.create_last_name;
-
-  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)";
-
-  getConnection().query(
-    queryString,
-    [firstName, lastName],
-    (err, results, fields) => {
-      if (err) {
-        console.log("Failed to insert new user: " + err);
-        res.sendStatus(500);
-        return;
-      }
-      console.log("Inserted a new user with id: ", results.insertedId);
-      res.end();
-    }
-  );
-});
-
-function getConnection() {
-  return mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    socket: "	/Applications/MAMP/tmp/mysql/mysql.sock",
-    database: "myDataBase"
-  });
-}
-
-app.get("/user/:id", (req, res) => {
-  console.log("Fetching user with id: " + req.params.id);
-
-  const userId = req.params.id;
-  const queryString = "SELECT * FROM users WHERE id = ?";
-
-  getConnection().query(queryString, [userId], (err, rows, fields) => {
-    console.log("I think we fetched users successfully");
-    if (err) {
-      console.log("Failed to query for users: " + err);
-      res.sendStatus(500);
-      return;
-    }
-    console.log("Connected");
-    res.json(rows);
-  });
-});
+// refactoring our code
+// with Router
+const router = require("./routes/user.js");
+app.use(router);
 
 // main route landing page
 app.get("/", (req, res) => {
   console.log("Responding to root route");
   res.send("Hello from Root");
-});
-
-// getting all the users (all rows from the database table)
-app.get("/users", (req, res) => {
-  const queryString = "SELECT * FROM users";
-  getConnection().query(queryString, (err, rows, fields) => {
-    if (err) {
-      console.log("Failed to query for users: " + err);
-      res.sendStatus(500);
-      return;
-    }
-    res.json(rows);
-  });
 });
 
 // localhost:3003
