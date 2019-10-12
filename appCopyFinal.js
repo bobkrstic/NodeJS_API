33ff1e2a337fc056fb7ec6adef83485c316592f8
bobkrstic <slobodankrsticnis@yahoo.com>
2019-10-11 21:23:42 -0700

    Saving a new file with comments and cleaning app.js

--- appCopyFinal.js
+++ appCopyFinal.js
@@ -1,10 +1,16 @@
+// SAVED THE ENTIRE FILE WITH ALL COMMENTS AND COMMENT OUTS
+// USING IT TO BETTER UNDERSTAND WHAT'S HAPPENING
+
 // load our app server using express
 var express = require("express");
 const app = express();
 const morgan = require("morgan");
 var mysql = require("mysql");
 
+// npm i body-parser
 const bodyParser = require("body-parser");
+// to make sure app uses body-parser
+// helps process the request easier
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // our application server is now going to able to serve all of the files
@@ -20,8 +26,18 @@ app.post("/user_create", (req, res) => {
   const firstName = req.body.create_first_name;
   const lastName = req.body.create_last_name;
 
-  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)";
+  // const connection = mysql.createConnection({
+  //   host: "localhost",
+  //   port: 3306,
+  //   user: "root",
+  //   password: "root",
+  //   socket: "	/Applications/MAMP/tmp/mysql/mysql.sock",
+  //   database: "myDataBase"
+  // });
 
+  // first_name last_name the way they are defined in the data base
+  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)";
+  // next step is the sequel query to use the input data and place it into the table (data base)
   getConnection().query(
     queryString,
     [firstName, lastName],
@@ -51,6 +67,15 @@ function getConnection() {
 app.get("/user/:id", (req, res) => {
   console.log("Fetching user with id: " + req.params.id);
 
+  // var connection = mysql.createConnection({
+  //   host: "localhost",
+  //   port: 3306,
+  //   user: "root",
+  //   password: "root",
+  //   socket: "	/Applications/MAMP/tmp/mysql/mysql.sock",
+  //   database: "myDataBase"
+  // });
+
   const userId = req.params.id;
   const queryString = "SELECT * FROM users WHERE id = ?";
 
@@ -74,6 +99,15 @@ app.get("/", (req, res) => {
 
 // getting all the users (all rows from the database table)
 app.get("/users", (req, res) => {
+  // const connection = mysql.createConnection({
+  //   host: "localhost",
+  //   port: 3306,
+  //   user: "root",
+  //   password: "root",
+  //   socket: "	/Applications/MAMP/tmp/mysql/mysql.sock",
+  //   database: "myDataBase"
+  // });
+
   const queryString = "SELECT * FROM users";
   getConnection().query(queryString, (err, rows, fields) => {
     if (err) {
